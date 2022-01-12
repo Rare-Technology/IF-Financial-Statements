611d6e68e551c4a0916ae38b90c2dc4fa5bf3d04
ibidyouadu <angel.d.umana@gmail.com>
2022-01-12 01:06:58 -0500

    add settings template and registration page; closes #1, closes #14

--- mysite/statements/views.py
+++ mysite/statements/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+    if request.method == "post":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            messages.success(request, "Registratin successful. Hello %s!" % username)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+        ctx = {'form': form}
+    return render(request, 'registration/register.html', ctx)
+
 def settings(request):
-    return HttpResponse("This will be the account settings page !")
+    return render(request, 'settings.html')
 
 def home(request):
     return render(request, 'home.html')
